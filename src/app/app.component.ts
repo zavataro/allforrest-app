07ce1e2d4d1212c29b148e736dad736ec11825f7
Luiz Zavataro <zavataro@gmail.com>
2022-12-01 16:52:03 -0300

    Incluindo jwt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 
-export class AppComponent {
-  public authenticate: boolean = false;
+export class AppComponent implements OnInit {
+  public authenticate: boolean = false
+
+  constructor( private router: Router) { }
+
+  ngOnInit() {
+    localStorage.setItem('auth', JSON.stringify({"isLogged":"teste"}))
+    var novo = localStorage.getItem('auth')?.toString()
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
 
 import { ToolbarComponent } from './shared/toolbar.component';
 import { LoginComponent } from './auth/login.component';
